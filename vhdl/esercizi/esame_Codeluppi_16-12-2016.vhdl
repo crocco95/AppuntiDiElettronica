a1f1aac04b0a0643a5032131d111ca3b57dd42c0
Riccardo Quercetti <riccardo.quercetti.93@gmail.com>
2017-06-19 11:07:47 +0200

    [ADD] Implementazione VHDL 16-12-2016 terminata

--- vhdl/esercizi/esame_Codeluppi_16-12-2016.vhdl
+++ vhdl/esercizi/esame_Codeluppi_16-12-2016.vhdl
@@ -4,11 +4,11 @@ use IEE.std_logic_1164.all;
 
 -- entity
 entity esercizio_2 is
-	port(
-		input_1, input_0: in std_logic;
-		clock, reset: in std_logic;
-		output_1, output_0: out std_logic
-	);
+  port(
+    input_1, input_0: in std_logic;
+    clock, reset: in std_logic;
+    output_1, output_0: out std_logic
+  );
 end esercizio_2;
 
 -- architecture
@@ -16,10 +16,12 @@ architecture esercizio_2_architecture of esercizio_2 is
   type stato is (A, B, C, D)
   signal stato_corrente <= A, stato_successivo <= A : stato;
 begin
+
   latch_process : process(clock, reset)
+  begin
     if (reset = '1') then
-      stato_successivo := D;
-      stato_corrente := D;
+      stato_successivo := A;
+      stato_corrente := A;
     elsif (rising_edge(clock)) then
       stato_corrente := stato_successivo;
       case (stato_corrente) is
@@ -31,8 +33,18 @@ begin
     end if;
   end process sync_proc;
   
-  rete_combinatoria_process : process(input_1, input_0)
+  rete_combinatoria_process : process(input_1, input_0, stato_corrente)
   begin
-    
+    case stato_corrente is
+      when A =>
+        if ((input_1 and (not input_0)) or ((not input_1) and input_0)) then
+          stato_successivo <= B;
+        elsif (input_1 and input_0) then
+          stato_successivo <= C;
+        else
+          stato_successivo <= A;
+        end if;
+    end case;
   end process rete_combinatoria_process
+  
 end esercizio_2_architecture
